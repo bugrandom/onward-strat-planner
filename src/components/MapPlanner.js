4edc80fc0139a1364b1f1a481cf7d155b12f267e
bugrandom <112142092+bugrandom@users.noreply.github.com>
2025-08-18 02:01:30 -0400

    Update MapPlanner.js

--- src/components/MapPlanner.js
+++ src/components/MapPlanner.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import PlayerCircle from './PlayerCircle';
+
+export default function MapPlanner({ players }) {
+  return (
+    <svg width={800} height={600} style={{ background: '#333' }}>
+      {players.map((player) => (
+        <PlayerCircle key={player.id} piece={player} />
+      ))}
+    </svg>
+  );
+}
 
