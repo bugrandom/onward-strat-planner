4c098f0f89dcb72ce5a4986b58c690a379513aaf
bugrandom <112142092+bugrandom@users.noreply.github.com>
2025-08-17 22:10:48 -0400

    Update MapArea.js

--- src/components/MapArea.js
+++ src/components/MapArea.js
@@ -1,1 +1,40 @@
+import PlayerCircle from './PlayerCircle';
+import Marker from './Marker';
+
+const MAP_WIDTH = 800;
+const MAP_HEIGHT = 600;
+
+const MapArea = ({ selectedMap, redPieces, bluePieces, markers, updatePiecePosition, updateMarkerPosition, handleMapClick }) => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>{selectedMap.name} Strat Planner</h1>
+      <svg
+        width={MAP_WIDTH}
+        height={MAP_HEIGHT}
+        style={{
+          border: '3px solid #555',
+          borderRadius: 10,
+          backgroundImage: `url(${selectedMap.image})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          display: 'block',
+          userSelect: 'none',
+        }}
+        onClick={handleMapClick}
+      >
+        {redPieces.map((p) => (
+          <PlayerCircle key={p.id} piece={p} onDrag={updatePiecePosition} />
+        ))}
+        {bluePieces.map((p) => (
+          <PlayerCircle key={p.id} piece={p} onDrag={updatePiecePosition} />
+        ))}
+        {markers.map((m) => (
+          <Marker key={m.id} marker={m} onDrag={updateMarkerPosition} />
+        ))}
+      </svg>
+    </div>
+  );
+};
+
+export default MapArea;
 
