cd86da70958b61333add42e3ab6b0271d0a15df8
bugrandom <112142092+bugrandom@users.noreply.github.com>
2025-08-18 02:02:08 -0400

    Update RoleSelector.js

--- src/components/RoleSelector.js
+++ src/components/RoleSelector.js
@@ -1,1 +1,16 @@
+import React from 'react';
+
+const roles = ['Rifleman', 'Specialist', 'Support', 'Marksman'];
+
+export default function RoleSelector({ selectedRole, onSelect }) {
+  return (
+    <select value={selectedRole} onChange={(e) => onSelect(e.target.value)}>
+      {roles.map((role) => (
+        <option key={role} value={role}>
+          {role}
+        </option>
+      ))}
+    </select>
+  );
+}
 
