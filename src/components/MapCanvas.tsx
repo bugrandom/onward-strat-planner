650ca4f7d1f065a9e7acdb6b5c31e243b95699c8
bugrandom <112142092+bugrandom@users.noreply.github.com>
2025-08-18 02:26:51 -0400

    Update MapCanvas.tsx

--- src/components/MapCanvas.tsx
+++ src/components/MapCanvas.tsx
@@ -1,1 +1,17 @@
+import IconOverlay from './IconOverlay';
+import mapImage from '../assets/map.png';
+
+interface Props {
+  selectedRole: string;
+}
+
+const MapCanvas = ({ selectedRole }: Props) => {
+  return (
+    <div className="relative w-full h-[600px] bg-cover bg-center" style={{ backgroundImage: `url(${mapImage})` }}>
+      <IconOverlay role={selectedRole} />
+    </div>
+  );
+};
+
+export default MapCanvas;
 
