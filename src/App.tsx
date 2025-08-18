0214f73babeb7f65542104a4bfcd609c660839b1
bugrandom <112142092+bugrandom@users.noreply.github.com>
2025-08-18 03:28:41 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import MapCanvas from './components/MapCanvas';
 import ShapePalette from './components/ShapePalette';
+import DropLayer from './components/DropLayer';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex">
-      {/* Sidebar with draggable shapes */}
       <ShapePalette />
-
-      {/* Main map area */}
-      <div className="flex-1 flex items-center justify-center">
+      <div className="relative flex-1 flex items-center justify-center">
         <MapCanvas />
+        <DropLayer />
       </div>
     </div>
   );
